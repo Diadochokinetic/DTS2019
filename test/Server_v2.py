66213ea384214eb8c441ccda6538e95ee94d0d28
Fabian Eckstein <diadochokinetic@gmail.com>
2019-10-12 20:48:42 +0200

    test

--- test/Server_v2.py
+++ test/Server_v2.py
@@ -92,7 +92,7 @@ class SocketServer:
                 #client_sock.send(message_binary)
                 #x += 1
                 #check if connection is still there
-                client_sock, client_addr = self.sock.accept()
+                
 
 
             else:
